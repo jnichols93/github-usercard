3c5d60c256a40592b7271e1bf7d17f2e672f77f5
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-08-08 15:22:40 -0500

    card creayor made

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,12 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const cards = document.querySelector('.cards');
 
+axios.get('https://api.github.com/users/jnichols93') .then(myData => {
+  const myCard = cardMaker(myData.data);
+  cards.append(myCard);
+});
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +29,17 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [ 
+  tetondan,
+  dustinmyers,
+  justsml,
+  luishrd,
+  bigknell];
+  followersArray.forEach(handle =>{
+    axios.get('https://api.github.com/users/'+handle).then(myInfo =>{const newCard = createCard(myInfo.data);
+  cards.append(myInfo);
+});
+  });
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +68,49 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+function cardMaker(i){
+  ///components
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const profileAlink = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+  ///class assignments
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+
+  name.textContent = i.name;
+  image.src = obj.avatar_url;
+  userName.textContent = i.login;
+  location.textContent = `Location:${i.location}`;
+  profile.textContent = `Profile`;
+  profileAlink.href = i.html_url;
+  profileAlink.textContent = `i.html_url`;
+  followers.textContent = `Followers: ${i.followers}`;
+  following.textContent = `following: ${i.following}`;
+  bio.textContent = `Bio ${i.bio}`;
+
+  //appending
+  card.append(image);
+  card.append(CardInfo);
+  cardInfo.append(name);
+  cardInfo.append(userName);
+  cardInfo.append(location);
+  cardInfo.append(profile);
+  profile.append(profile);
+  profile.append(profileAlink);
+  cardInfo.append(followers);
+  cardInfo.append(following);
+  cardInfo.append(bio);
+
+return card;
+}
